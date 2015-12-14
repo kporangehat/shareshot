c9c1e48b16085b1c4e0ceba30221f05cde237497
KP <kp@shotgunsoftware.com>
2015-12-14 16:33:39 -0600

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn shareshot.wsgi --log-file -
---
+++ shareshot/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for shareshot project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shareshot.settings")
+#
+# application = get_wsgi_application()
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
